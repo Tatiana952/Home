3276baf29b82f189c303d897569570aeae88555d
Tatiana <lulu.kep@yandex.ru>
2022-07-15 23:55:50 +0400

    023

---
+++ 023/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] a = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    a[i] = Math.Pow((i+1),3);
+    Console.Write(a[i] + " ");
+}
