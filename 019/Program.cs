ed654081cb4cc103731fcbf6635d0bcc2ce458e5
Tatiana <lulu.kep@yandex.ru>
2022-07-15 23:08:08 +0400

    019

---
+++ 019/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a five digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n > 99999 || n < 10000)
+{
+    Console.WriteLine("Enter a five digit number: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+string n1 = Convert.ToString(n);
+
+if (n1[0] == n1[4] && n1[1] == n1[3])
+    Console.WriteLine("It's a palindrome");
+else
+    Console.WriteLine("It's not a palindrome");
