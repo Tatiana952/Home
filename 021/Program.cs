3835b5f995106e589f4e8806b3c06ba90301eefe
Tatiana <lulu.kep@yandex.ru>
2022-07-15 23:35:08 +0400

    021

---
+++ 021/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] a = new int[3];
+int[] b = new int[3];
+int i = 0;
+
+Console.WriteLine("Enter 3 coordinates of the pont A: ");
+for (i=0; i<3; i++)
+    a[i]=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter 3 coordinates of the pont B: ");
+for (i=0; i<3; i++)
+    b[i]=Convert.ToInt32(Console.ReadLine());
+
+double d1 = 0;
+for (i=0; i<3; i++)
+d1 = d1 + Math.Pow((a[i]-b[i]),2);
+
+Console.WriteLine();
+Console.WriteLine("Distance between points is: "+ "{0:f2}", Math.Sqrt(d1));
